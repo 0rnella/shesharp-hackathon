06e907a12aebbbd0d43d5a93e87a4f63426b67af
friggito <44591279+0rnella@users.noreply.github.com>
2023-06-10 17:34:43 +0200

    get analysis

--- src/app/InputBox.js
+++ src/app/InputBox.js
@@ -2,34 +2,7 @@
 import React from 'react';
 import styles from './page.module.css';
 
-const InputBox = () => {
-
-  const handleSubmit = async (event) => {
-    // Stop the form from submitting and refreshing the page.
-    event.preventDefault()
-
-    // Get data from the form.
-    const data = {
-      url: event.target.url.value,
-    }
-
-    // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify(data)
-    const endpoint = 'https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/sourcestack-data-global.json'
-
-
-    const response = await fetch(endpoint)
-    const urlData = await response.json()
-
-    const value = urlData['data'].forEach(element => {
-      if (data['url'] === (element['post_url'])) {
-        console.log("We found you!")
-      }
-    });
-
-    // console.log(urlData)
-  }
-
+const InputBox = ({handleSubmit}) => {
   return (
     <form onSubmit={handleSubmit}>
       <input
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,71 @@
-// import React, { useState } from 'react';
-import Image from 'next/image'
-import styles from './page.module.css'
-import React from 'react';
-import InputBox from './InputBox';
-
-
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
+import React from "react";
+import InputBox from "./InputBox";
+import Link from "next/link";
 
 export default function Home() {
+  const [sourceStackJob, setSourceStackJob] = useState();
+
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    // Get data from the form.
+    const url = event.target.url.value;
+
+    const endpoint =
+      "https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/sourcestack-data-global.json";
+
+    const response = await fetch(endpoint);
+    const sourceStackJobs = await response.json();
+
+    sourceStackJobs["data"].forEach((element) => {
+      if (url === element["post_url"]) {
+        console.log("We found you!");
+        setSourceStackJob(element);
+      }
+    });
+  };
+
+  const JobAnalysis = ({ sourceStackJob }) => {
+    console.log(sourceStackJob);
+    const tagsMatched = sourceStackJob["tags_matched"];
+    const categories = sourceStackJob["tag_categories"];
+    return (
+      <div>
+        <p>SourceStack has analyzed your job and found the following:</p>
+        <h2>Title: {sourceStackJob['job_name']}</h2>
+        <p> <b>{tagsMatched.length} matched tags:</b> {tagsMatched.join(', ')}</p>
+
+          {tagsMatched.length > 3 && (
+            <span>That's a lot of technologies. Are you sure you need all them? If not, consider removing the keywords.</span>
+          )}
+          <p> <b>{categories.length} matched categories:</b> {categories.join(', ')}</p>
+        <p>
+          {categories.length > 3 && (
+            <span>That's a lot of categories. Are you sure you need all them? If not, consider removing the keywords.</span>
+          )}
+        </p>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <Link href="/form">
+          Let's go further. Make your posting better with AI!
+        </Link>
+      </div>
+    );
+  };
 
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-
         <div>
-          <a
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            {" "}
             <Image
               src="/logo.png"
               alt="Vercel Logo"
@@ -36,10 +83,13 @@ export default function Home() {
         <div className={styles.banner}>
           <h1>We help you make job posts better</h1>
           <p></p>
-          <InputBox />
+          {sourceStackJob ? (
+            <JobAnalysis sourceStackJob={sourceStackJob} />
+          ) : (
+            <InputBox handleSubmit={handleSubmit} />
+          )}
         </div>
       </div>
-
     </main>
-  )
+  );
 }
